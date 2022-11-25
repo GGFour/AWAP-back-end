daa6a3f5c06956fcd7aa4831f4ccfc0b5420f213
Quyen Nguyen <quyen.ng1902@gmail.com>
2022-11-25 11:06:49 +0200

    Added sources and descriptions for each VIS
    Updated script

--- data/dataloader.js
+++ data/dataloader.js
@@ -42,8 +42,9 @@ let config = [
     {
         name: 'Global historical surface temperature anomalies from January 1850 onwards',
         tablename: 'visualization1',
-        description: '',
-        source: '',
+        description:
+            'The gridded data are a blend of the land-surface air temperature dataset and the sea-surface temperature anomalies relative to a 1961-1990 reference period. Data are available for each month from January 1850 onwards, on a 5 degree grid and as global and regional average time series. The dataset is a collaborative product of the Met Office Hadley Centre and the Climatic Research Unit at the University of East Anglia ',
+        source: 'https://www.metoffice.gov.uk/hadobs/hadcrut5/',
         keys: {
             x: 'Time',
             y: 'Anomaly (deg C)',
@@ -55,8 +56,9 @@ let config = [
     {
         name: 'Northern Hemisphere 2,000-year temperature reconstruction',
         tablename: 'visualization2',
-        description: '',
-        source: '',
+        description:
+            'Northern Hemisphere temperature reconstruction for the past 2,000 years by combining low-resolution proxies with tree-ring data, using a wavelet transform technique to achieve timescale-dependent processing of the data. Link to full case study, which includes data measurements description - https://www.nature.com/articles/nature03265',
+        source: 'https://bolin.su.se/data/moberg-2012-nh-1?n=moberg-2005',
         keys: {
             x: 'Year',
             y: 'T',
@@ -66,8 +68,12 @@ let config = [
     {
         name: 'Atmospheric CO2 concentrations from Mauna Loa measurements starting 1958',
         tablename: 'visualization3',
-        description: '',
-        source: '',
+        description: `CO2 concentrations measurements made at the Mauna Loa Observatory reflect truth about our global atmosphere. The main reasons for that confidence are:
+                        1. The Observatory near the summit of Mauna Loa, at an altitude of 3400 m, is well situated to measure air masses that are representative of very large areas.
+                        2. All of the measurements are rigorously and very frequently calibrated.
+                        3. Ongoing comparisons of independent measurements at the same site allow an estimate of the accuracy, which is generally better than 0.2 ppm.
+                        Link - https://gml.noaa.gov/ccgg/about/co2_measurements.html `,
+        source: 'https://gml.noaa.gov/ccgg/trends/data.html',
         keys: {
             x: 'date',
             y: 'mean',
@@ -78,8 +84,8 @@ let config = [
     {
         name: 'Antarctic Ice Core records of atmospheric CO2 ratios combined with Mauna Loa measurements',
         tablename: 'visualization4',
-        description: '',
-        source: '',
+        description: `The CO2 records presented here are derived from three ice cores obtained at Law Dome, East Antarctica from 1987 to 1993. The Law Dome site satisfies many of the desirable characteristics of an ideal ice core site for atmospheric CO2 reconstructions including negligible melting of the ice sheet surface, low concentrations of impurities, regular stratigraphic layering undisturbed at the surface by wind or at depth by ice flow, and high snow accumulation rate. Further details on the site, drilling, and cores are provided in Etheridge et al. (1996), Etheridge and Wookey (1989), and Morgan et al (1997).`,
+        source: 'https://cdiac.ess-dive.lbl.gov/ftp/trends/co2/lawdome.combined.dat',
         keys: {
             x: 'airAgeAD',
             y: 'CO2MixingRatioPpm',
@@ -90,8 +96,9 @@ let config = [
     {
         name: 'Vostok Ice Core CO2 measurements, 417160 - 2342 years',
         tablename: 'visualization5',
-        description: '',
-        source: '',
+        description:
+            'In January 1998, the collaborative ice-drilling project at the Russian Vostok station in East Antarctica yielded the deepest ice core ever recovered, reaching a depth of 3,623 m (Petit et al. 1997, 1999). ',
+        source: 'https://cdiac.ess-dive.lbl.gov/ftp/trends/co2/vostok.icecore.co2',
         keys: {
             x: 'Mean age of the air (yr BP)',
             y: 'CO2 concentration',
@@ -101,8 +108,9 @@ let config = [
     {
         name: 'Ice core 800k year composite study CO2 measurements',
         tablename: 'visualization6',
-        description: '',
-        source: '',
+        description:
+            'The European Project for Ice Coring in Antarctica Dome ice core from Dome C (EDC) has allowed for the reconstruction of atmospheric CO2 concentrations for the last 800,000 years.',
+        source: 'https://www.ncei.noaa.gov/pub/data/paleo/icecore/antarctica/antarctica2015co2composite.txt',
         keys: {
             x: 'Age, gas, calendar years before present (y)',
             y: 'CO2 concentration (ppm)',
@@ -112,8 +120,8 @@ let config = [
     {
         name: 'Evolution of global temperature over the past two million years',
         tablename: 'visualization7',
-        description: '',
-        source: '',
+        description: `Here I present a spatially weighted proxy reconstruction of global temperature over the past 2 million years estimated from a multi-proxy database of over 20,000 sea surface temperature point reconstructions. Global temperature gradually cooled until roughly 1.2 million years ago and cooling then stalled until the present.`,
+        source: 'http://carolynsnyder.com/papers/Snyder_Data_Figures.zip',
         keys: {
             x: 'Time (kyr BP)',
             y: '50%',
@@ -123,8 +131,9 @@ let config = [
     {
         name: 'CO2 emissions by country',
         tablename: 'visualization8',
-        description: '',
-        source: '',
+        description:
+            'Stacked line chart showing CO2 emissions of each country from 1959 to 2021',
+        source: 'https://data.icos-cp.eu/licence_accept?ids=%5B%22lApekzcmd4DRC34oGXQqOxbJ%22%5D',
         keys: {
             x: 'year',
             y: 'CO2_Mt',
@@ -135,8 +144,9 @@ let config = [
     {
         name: 'CO2 emissions by sectors',
         tablename: 'visualization9',
-        description: '',
-        source: '',
+        description:
+            'The pie chart shows the breakdown of global greenhouse gas emissions in 2016. This is the latest breakdown of global emissions by sector and its sub-sectors, published by Climate Watch and the World Resources Institute.',
+        source: 'https://ourworldindata.org/uploads/2020/09/Global-GHG-Emissions-by-sector-based-on-WRI-2020.xlsx',
         keys: {
             sector: 'Sector',
             subsector: 'Sub-sub-sector',
@@ -169,8 +179,8 @@ async function processRow(row, client, conf, keymap, keys, parameters) {
             keys.map((key) => row[keymap[key]])
         )
     } catch (err) {
-        process.out.writeln(err.stack)
-        process.out.writeln(row)
+        process.stdout.write(err.stack)
+        process.stdout.write(row)
         process.exit(1)
     }
 }
@@ -185,8 +195,8 @@ async function insertIntoVisualisations(client, conf) {
             conf.source,
         ])
     } catch (err) {
-        process.out.writeln(err.stack)
-        process.out.writeln(row)
+        process.stdout.write(err.stack)
+        process.stdout.write(row)
         process.exit(1)
     }
 }
@@ -206,7 +216,7 @@ async function load(config) {
             await processRow(row, client, conf, keymap, keys, parameters)
         }
         insertIntoVisualisations(client, conf)
-        process.out.writeln(`table: ${conf.tablename} finished`)
+        process.stdout.write(`table: ${conf.tablename} finished`)
     }
 
     process.exit(1)

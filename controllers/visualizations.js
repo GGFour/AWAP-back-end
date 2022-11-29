54e80a18b2c05f016e669efe380bd2fb09cfe6ce
Vladimir Surtaev <t1suvl00@students.oamk.fi>
2022-11-29 23:37:49 +0200

    Add endpoint for getting common visualizations data
    Modify getting single visualization api

--- controllers/visualizations.js
+++ controllers/visualizations.js
@@ -5,12 +5,26 @@ const pool = require('../model/pool')
 
 async function getVisualizationData(visId) {
     let visData = await pool.query(`SELECT * FROM visualizations WHERE id=$1`, [
-        visId
+        visId,
     ])
-    let data = await pool.query(`SELECT * FROM visualization${visId}`)
+    let data = await pool.query(`SELECT * FROM ${visData.rows[0].table_name}`)
     let result = visData.rows[0]
     result['data'] = data.rows
     return result
 }
 
-module.exports = getVisualizationData
+async function getVisualizations() {
+    let visualizationsData = await pool.query(
+        `SELECT * FROM visualizations`,
+        []
+    )
+    let visualizationsRows = visualizationsData.rows
+    for (let i = 0; i < visualizationsRows.length; ++i) {
+        let visualization = visualizationsRows[i]
+        let data = await pool.query(`SELECT * FROM ${visualization.table_name}`)
+        visualization['data'] = data.rows
+    }
+    return visualizationsRows
+}
+
+module.exports = { getVisualizationData, getVisualizations }

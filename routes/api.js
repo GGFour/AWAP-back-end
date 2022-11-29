87e2c2e3435c2433c858d6f6b66dd58cacf6f455
Vladimir Surtaev <t1suvl00@students.oamk.fi>
2022-11-29 23:37:49 +0200

    Add endpoint for getting common visualizations data
    Modify getting single visualization api

--- routes/api.js
+++ routes/api.js
@@ -2,18 +2,16 @@
  * This file contains routes for getting visualizations data
  */
 let express = require('express')
-let getVisualizationData = require('../controllers/visualizations')
+let {
+    getVisualizationData,
+    getVisualizations,
+} = require('../controllers/visualizations')
 let router = express.Router()
 
 /* GET visualizations data */
-router.get('/visualizations', function (req, res, next) {
-    res.render('index', { title: 'Express' })
-})
-
-/* GET visualization1 data */
-router.get('/visualization1', async function (req, res, next) {
+router.get('/visualizations', async function (req, res, next) {
     try {
-        let data = await getVisualizationData(1)
+        let data = await getVisualizations()
         return res.status('200').json(data)
     } catch (e) {
         console.error(e)
@@ -21,87 +19,10 @@ router.get('/visualization1', async function (req, res, next) {
     }
 })
 
-/* GET visualization2 data */
-router.get('/visualization2', async function (req, res, next) {
+/* GET visualization x data */
+router.get('/visualization', async function (req, res, next) {
     try {
-        let data = await getVisualizationData(2)
-        return res.status('200').json(data)
-    } catch (e) {
-        console.error(e)
-        return res.status('500').json({ message: 'Im not working' })
-    }
-})
-
-/* GET visualization3 data */
-router.get('/visualization3', async function (req, res, next) {
-    try {
-        let data = await getVisualizationData(3)
-        return res.status('200').json(data)
-    } catch (e) {
-        console.error(e)
-        return res.status('500').json({ message: 'Im not working' })
-    }
-})
-
-/* GET visualization4 data */
-router.get('/visualization4', async function (req, res, next) {
-    try {
-        let data = await getVisualizationData(4)
-        return res.status('200').json(data)
-    } catch (e) {
-        console.error(e)
-        return res.status('500').json({ message: 'Im not working' })
-    }
-})
-
-/* GET visualization5 data */
-router.get('/visualization5', async function (req, res, next) {
-    try {
-        let data = await getVisualizationData(5)
-        return res.status('200').json(data)
-    } catch (e) {
-        console.error(e)
-        return res.status('500').json({ message: 'Im not working' })
-    }
-})
-
-/* GET visualization6 data */
-router.get('/visualization6', async function (req, res, next) {
-    try {
-        let data = await getVisualizationData(6)
-        return res.status('200').json(data)
-    } catch (e) {
-        console.error(e)
-        return res.status('500').json({ message: 'Im not working' })
-    }
-})
-
-/* GET visualization7 data */
-router.get('/visualization7', async function (req, res, next) {
-    try {
-        let data = await getVisualizationData(7)
-        return res.status('200').json(data)
-    } catch (e) {
-        console.error(e)
-        return res.status('500').json({ message: 'Im not working' })
-    }
-})
-
-/* GET visualization8 data */
-router.get('/visualization8', async function (req, res, next) {
-    try {
-        let data = await getVisualizationData(8)
-        return res.status('200').json(data)
-    } catch (e) {
-        console.error(e)
-        return res.status('500').json({ message: 'Im not working' })
-    }
-})
-
-/* GET visualization9 data */
-router.get('/visualization9', async function (req, res, next) {
-    try {
-        let data = await getVisualizationData(9)
+        let data = await getVisualizationData(req.query.id)
         return res.status('200').json(data)
     } catch (e) {
         console.error(e)

ba7a2cdaed027bec04904351295357e0d5d8debf
Vladimir Surtaev <t1suvl00@students.oamk.fi>
2022-12-05 16:37:36 +0200

    Fix authorization

--- middleware/authorizer.js
+++ middleware/authorizer.js
@@ -1,23 +1,21 @@
 //this middleware fucntion checks if the token is valid
 const jwt = require('jsonwebtoken')
-require('dotenv').config()
 
 module.exports = async (req, res, next) => {
     try {
-        //1. destructing the token
-        //token is sent inside a request header.
-        const jwtToken = req.header()
-        //2.checking if a token exists
+        // Get token from 'Authorization' header.
+        // The format of the string in header is 'Bearer <token>'.
+        const jwtToken = req.get('Authorization').split(' ')[1]
+        // checking if a token exists
         if (!jwtToken) {
-            return res.status(403).json('Not Authorized')
+            throw Error()
         }
-        //3. checking if a token is valid (if exists)
-        const payload = jwt.verify(jwtToken, process.env.jwtSecret)
-        //req.user = payload (data)
+        // checking if a token is valid (if exists)
+        const payload = jwt.verify(jwtToken, process.env.TOKEN_SECRET)
         req.user = payload.user
         next()
     } catch (err) {
         console.error(err.message)
-        return res.status(403).json('Not Authorized')
+        return res.status(403).json({ message: 'Not Authorized' })
     }
 }
--- utils/jwtGenerator.js
+++ utils/jwtGenerator.js
@@ -1,17 +1,12 @@
 const jwt = require('jsonwebtoken')
-//allow access to all environment variables
-require('dotenv').config()
 
-function jwtGenerator(id) {
+function jwtGenerator(user) {
     const payload = {
         user: {
-            id: id,
             username: user.username,
         },
-        // username: user.username,
-        // id: user.id,
     }
-    return jwt.sign(payload, process.env.jwtSecret, { expiresIn: '5hr' })
+    return jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: '5hr' })
 }
 
 module.exports = jwtGenerator

023d693c3092bc3c16d2026b019970bafadff152
Quyen Nguyen <quyen.ng1902@gmail.com>
2022-12-12 17:24:23 +0200

    Refactor for production

--- model/pool.js
+++ model/pool.js
@@ -4,8 +4,8 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
+    host: process.env.POSTGRES_HOST || 'localhost',
     user: process.env.POSTGRES_USER,
-    host: 'localhost',
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: 5432,
